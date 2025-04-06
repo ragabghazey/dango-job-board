6ed7e0df8277a855e793807ee656ce8a40807b8c
ragab ghazey <ragabghazey@gmail.com>
2025-04-06 14:13:52 +0200

    add jop

--- job/models.py
+++ job/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+"""
+django model filed 
+
+html 
+validation
+db size
+
+"""
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
